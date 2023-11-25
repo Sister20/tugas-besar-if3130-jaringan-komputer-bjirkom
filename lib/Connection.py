2e33eccda6ea511ed32670e88969f4b4135d39ed
hanifmz07 <13521157@std.stei.itb.ac.id>
2023-11-26 02:21:48 +0700

    feat: add is_server parameter in connection

--- lib/Connection.py
+++ lib/Connection.py
@@ -1,35 +1,39 @@
 import socket
 import sys
-from constant import MAX_SEGMENT
+from .constant import MAX_SEGMENT
+
 
 class Connection:
-  def __init__(self, ip : str,  port : int):
-    self.ip = ip
-    self.port = port
+    def __init__(self, ip: str, port: int, is_server: bool):
+        self.ip = ip
+        self.port = port
 
-    # set the socker to datagram
-    self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    # set options to reuse address and port
-    self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # set the socker to datagram
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    if (sys.platform != 'win32'):
-      self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    
-    self.socket.bind((self.ip, self.port))
-    print(f"[!] Server started at {self.ip}:{self.port}")
+        # set options to reuse address and port
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-  def listenMsg(self):
-    while (True):
-      bytesAddressPair = self.socket.recvfrom(MAX_SEGMENT)
-      
-      msg = bytesAddressPair[0]
-      address = bytesAddressPair[1]
+        if sys.platform != "win32":
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-      return msg, address
+        self.socket.bind((self.ip, self.port))
+        if is_server:
+            print(f"[!] Server started at {self.ip}:{self.port}")
+        else:
+            print(f"[!] Client started at {self.ip}:{self.port}")
 
-  def sendMsg(self, msg, dest):
-    self.socket.sendto(msg, dest)
+    def listenMsg(self):
+        while True:
+            bytesAddressPair = self.socket.recvfrom(MAX_SEGMENT)
 
-  def closeSocket(self):
-    self.socket.close()
+            msg = bytesAddressPair[0]
+            address = bytesAddressPair[1]
+
+            return msg, address
+
+    def sendMsg(self, msg, dest):
+        self.socket.sendto(msg, dest)
+
+    def closeSocket(self):
+        self.socket.close()
