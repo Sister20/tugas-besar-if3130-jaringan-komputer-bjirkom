159aa121483d42c2e5b253701485c955d817085c
hanifmz07 <13521157@std.stei.itb.ac.id>
2023-11-26 02:24:29 +0700

    feat: implement three way handshake

---
+++ client.py
@@ -0,0 +1,43 @@
+from lib.Connection import Connection
+from lib.clientparser import ClientParser
+from lib.Segment import Segment
+from lib.flags import Flags
+from lib.constant import *
+
+
+class Client:
+    def __init__(self):
+        self.parser = ClientParser()
+        self.client_port, self.broadcast_port, self.output_path = self.parser.get_args()
+        self.connection = Connection(
+            ip="127.0.0.1",
+            port=self.client_port,
+            is_server=False,
+        )
+        self.segment = Segment()
+
+    # TODO: Add timeout implementation
+    def three_way_handshake(self):
+        print("[Handshake] Initiating three way handshake...")
+        while True:
+            print(f"[Handshake] Sending SYN request to 127.0.0.1:{self.client_port}")
+            self.segment = Flags.syn(seq_num=100)
+            self.connection.sendMsg(
+                self.segment.generate_bytes(), ("127.0.0.1", self.broadcast_port)
+            )
+            print("[Handshake] Waiting server response...")
+            _, address = self.connection.listenMsg()
+            print(f"[Handshake] Received SYN-ACK response from server {address}")
+            print(f"[Handshake] Sending ACK response to {address}")
+            self.segment = Flags.ack(seq_num=101, ack_num=301)
+            self.connection.sendMsg(
+                self.segment.generate_bytes(), ("127.0.0.1", self.broadcast_port)
+            )
+            break
+        print("Connection established")
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.three_way_handshake()
+    client.connection.closeSocket()
---
+++ server.py
@@ -0,0 +1,38 @@
+from lib.Connection import Connection
+from lib.serverparser import ServerParser
+from lib.Segment import Segment
+from lib.flags import Flags
+from lib.constant import MAX_SEGMENT
+
+
+class Server:
+    def __init__(self):
+        self.parser = ServerParser()
+        self.broadcast_port, self.input_path = self.parser.get_args()
+        self.connection = Connection(
+            ip="127.0.0.1",
+            port=self.broadcast_port,
+            is_server=True,
+        )
+        self.segment = Segment()
+
+    # TODO: Add timeout implementation
+    def three_way_handshake(self):
+        while True:
+            print("[Handshake] Waiting for clients...")
+            _, address = self.connection.listenMsg()
+            print(f"[Handshake] Received SYN request from client {address}")
+            print(f"[Handshake] Sending SYN-ACK response to {address}")
+            self.segment = Flags.syn_ack(seq_num=300, ack_num=101)
+            self.connection.sendMsg(self.segment.generate_bytes(), ("127.0.0.1", 50001))
+            print("[Handshake] Waiting client response...")
+            _, address = self.connection.listenMsg()
+            print("[Handshake] Received ACK flag from client")
+            break
+        print("Connection established")
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.three_way_handshake()
+    server.connection.closeSocket()
